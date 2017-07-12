e02ba65a9f4738498e74bf8741f073efa8e38bb4
Artem Lipatkin <lipatkin96@gmail.com>
2017-07-12 17:10:03 +0700

    Add containsAll and containsAny to IPlenty. Some tests work fine

--- src/intervals/iplenty.h
+++ src/intervals/iplenty.h
@@ -5,6 +5,14 @@ template <class T>
 class IPlenty {
 public:
 	virtual bool contains(const T& element) const = 0;
+
+	bool containsAll(T element) { return contains(element); }
+	template <class... Args>
+	bool containsAll(T element, Args... args) { return contains(element) && containsAll(args...); }
+
+	bool containsAny(T element) { return contains(element); }
+	template <class... Args>
+	bool containsAny(T element, Args... args) { return contains(element) || containsAny(args...); }
 };
 
 #endif //IPLENTY_H
