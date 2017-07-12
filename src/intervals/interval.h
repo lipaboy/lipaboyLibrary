9fd966d5faa7813f4c2d7929a5821c68b68de10a
Artem Lipatkin <lipatkin96@gmail.com>
2017-07-12 14:24:52 +0700

    Add TODO

--- src/intervals/interval.h
+++ src/intervals/interval.h
@@ -4,22 +4,24 @@
 //TODO: may be can create such class that would get parameters like lambda
 //where you can give such labmda: [] (int x, int y) { return x < y; } and would inherit from that class
 
+//TODO: add Args&&
 
 #include "iplenty.h"
 
 namespace IntervalFuncs {
 
-	//TODO: such interface already exists (Set)
 
 template <class T>
 class Interval : public IPlenty<T>
 {
 public:
     Interval(const T& left1, const T& right1) : leftBorder(left1), rightBorder(right1) {}
-    virtual bool in(const T& element) const { return (element > leftBorder) && (element < rightBorder); }
+    bool in(const T& element) const { return (element > leftBorder) && (element < rightBorder); }
     bool out(const T& element) const { return !in(element); }
     bool outLeft(const T& element) const { return (element <= leftBorder); }
     bool outRight(const T& element) const { return (element >= rightBorder); }
+
+	virtual bool contains(const T& element) const { return in(element); }
 
 	const T& left() const { return leftBorder; }
 	const T& right() const {return rightBorder; }
