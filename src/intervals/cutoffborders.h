5be94f1d74cd1d5ca010f023d9b220a29df849de
Artem Lipatkin <lipatkin96@gmail.com>
2017-08-19 00:18:36 +0700

    Remove already done TODO

--- src/intervals/cutoffborders.h
+++ src/intervals/cutoffborders.h
@@ -5,8 +5,6 @@
 
 namespace LipaboyLib {
 
-	//TODO: move to another package (but what's package??)
-
 	template <typename T>
 	inline const T& cutOffBorder(const T& value, const T& rightBorder, const T& leftBorder) {
 		return ((value < leftBorder) ? leftBorder : ((value > rightBorder) ? rightBorder : value));
@@ -27,22 +25,6 @@ namespace LipaboyLib {
 		return ((border.outLeft(value)) ? border.left() : ((border.outRight(value)) ? border.right() : value));
 	}
 
-	//I don't see any necessary to specialize cutOffBorder for double
-
-	//template <>
-	//inline const double& cutOffBorder(const double& value, const double& rightBorder, const double& leftBorder) {
-	//	return ((value < leftBorder - epsilon) ? leftBorder : ((value > rightBorder + epsilon) ? rightBorder : value));
-	//}
-	//
-	//template <>
-	//inline const double& cutOffRightBorder(const double& value, const double& rightBorder) {
-	//	return ((value > rightBorder) ? rightBorder : value);
-	//}
-	//
-	//template <>
-	//inline const double& cutOffLeftBorder(const double& value, const double& leftBorder) {
-	//	return ((value < leftBorder) ? leftBorder : value);
-	//}
 }
 
 #endif //CUT_OFF_BORDERS_H
