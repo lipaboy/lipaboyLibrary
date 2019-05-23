42031544b2c87e6eafa6a0966290902e50612eae
Artem Lipatkin <lipatkin96@gmail.com>
2019-05-23 14:38:37 +0700

    Add class BigInteger from external library.

---
+++ src/long_numbers/big_integer_library.h
@@ -0,0 +1,8 @@
+// This header file includes all of the library header files.
+
+#include "number_like_array.h"
+#include "big_unsigned.h"
+#include "big_integer.h"
+#include "big_integer_algorithms.h"
+#include "big_unsigned_in_a_base.h"
+#include "big_integer_utils.h"
