3b2c47318dcf7a0abde9f77bdba245f29b3ad0c3
lipatkin96 <lipatkin96@gmail.com>
2019-07-08 08:24:14 +0700

    (Stream) add results for benchmarks tests in comments.

--- tests/stream/benchmarks/stream_vs_fast_stream.cpp
+++ tests/stream/benchmarks/stream_vs_fast_stream.cpp
@@ -21,8 +21,10 @@ namespace stream_benchmarks {
 
 	// Results: (Windows)
 	// 7% boost and that's all
-	TEST(Benchmark_stream_vs_fast_stream, DISABLED_skip) {
-		const size_t SIZE = static_cast<size_t>(1e7);
+    // Results: (Linux, Release, 1e8)
+    // 0% boost (97 simple, 2717 stream, 2751 fast_stream)
+    TEST(Benchmark_stream_vs_fast_stream, DISABLED_skip) {
+        const size_t SIZE = static_cast<size_t>(1e8);
 		int sum1 = 0;
 		int sum3 = 0;
 		{
@@ -60,14 +62,18 @@ namespace stream_benchmarks {
 		}
 
 
-		ASSERT_EQ(sum1, sum3);
-		system("pause");
+        ASSERT_EQ(sum1, sum3);
+#ifdef WIN32
+        system("pause");
+#endif
 
 		ASSERT_FALSE(true);
 	}
 
-	TEST(Benchmark_stream_vs_fast_stream, DISABLED_skip_string) {
-		const size_t SIZE = static_cast<size_t>(8e5);
+    // Results: (Linux, Release, 1e7)
+    // 0% boost (1936 simple, 2307 stream, 2300 fast_stream)
+    TEST(Benchmark_stream_vs_fast_stream, DISABLED_skip_string) {
+        const size_t SIZE = static_cast<size_t>(1e7);
 		string sum1 = "";
 		string sum2 = "";
 		string sum3 = "";
@@ -93,14 +99,14 @@ namespace stream_benchmarks {
 
 			auto start = getCurrentTime();
 			int a = 0;
-			sum2 = std::move(buildStream([&a]() { return std::to_string(a++); }) 
+            sum2 = buildStream([&a]() { return std::to_string(a++); })
 				| get(SIZE) 
 				| get(SIZE)
 				| get(SIZE)
 				| skip(100) 
 				| skip(100)
 				| skip(100)
-				| sum());
+                | sum();
 			cout << "Time: " << diffFromNow(start) << " Stream" << endl;
 		}
 
@@ -110,35 +116,38 @@ namespace stream_benchmarks {
 
 			auto start = getCurrentTime();
 			int a = 0;
-			sum3 = std::move(buildStream([&a]() { return std::to_string(a++); })
+            sum3 = buildStream([&a]() { return std::to_string(a++); })
 				| get(SIZE) 
 				| get(SIZE)
 				| get(SIZE)
 				| skip(100) 
 				| skip(100)
 				| skip(100)
-				| sum());
+                | sum();
 			cout << "Time: " << diffFromNow(start) << " Fast Stream" << endl;
 		}
 
-		system("pause");
+#ifdef WIN32
+        system("pause");
+#endif
 
 		//ASSERT_EQ(sum1, sum2);
 		//ASSERT_EQ(sum3, sum2);
 		ASSERT_FALSE(true);
 	}
 
-	// Results: (Windows)
-	// 
-	TEST(Benchmark_stream_vs_fast_stream, DISABLED_filter) {
-		const size_t SIZE = static_cast<size_t>(1e7);
-		auto filterFunc = [] (int i) { return rand() % 2 == 0; };
+    // Results: (Linux, Release, 1e8)
+    // 23% boost, deceleration by using stream 30%
+    // (1347 simple, 5698 stream, 4358 fast_stream, 4681 short_stream)
+    TEST(Benchmark_stream_vs_fast_stream, DISABLED_filter) {
+        const size_t SIZE = static_cast<size_t>(1e8);
+        auto filterFunc = [] (int) { return rand() % 2 == 0; };
 
 		{
 			auto start = getCurrentTime();
 			int sum = 0;
 			for (size_t i = 0; i < SIZE; i++)
-				if (filterFunc(i))
+                if (filterFunc(int(i)))
 					sum += i;
 			cout << "Time: " << diffFromNow(start) << " Simple" << endl;
 		}
@@ -160,7 +169,7 @@ namespace stream_benchmarks {
 
 			auto sum1 = 
 				buildStream(1, 2, 3, 4, 5, 6) 
-				| filter([filterFunc](auto l) { return l % 2 == 0; }) | sum();
+                | filter([](auto l) { return l % 2 == 0; }) | sum();
 			ASSERT_EQ(sum1, 12);
 
 			auto start = getCurrentTime();
@@ -176,7 +185,7 @@ namespace stream_benchmarks {
 
 			auto stream =
 				(buildShortStream(1, 2, 3, 4, 5, 6)
-					| filter([filterFunc](auto l) -> bool { return l % 2 == 0; }));
+                    | filter([](auto l) -> bool { return l % 2 == 0; }));
 			auto sum1 = stream | sum();
 			ASSERT_EQ(sum1.value(), 12);
 
@@ -187,15 +196,18 @@ namespace stream_benchmarks {
 			cout << "Time: " << diffFromNow(start) << " Short Stream" << endl;
 		}
 
-		system("pause");
+#ifdef WIN32
+        system("pause");
+#endif
 
 		ASSERT_FALSE(true);
 	}
 
-	// Results: (Windows)
-	// 
-	TEST(Benchmark_stream_vs_fast_stream, DISABLED_filter_string) {
-		const size_t SIZE = static_cast<size_t>(8e5);
+    // Results: (Linux, Release, 1e7)
+    // 0% boost, deceleration by using stream 0%
+    // (3005 simple, 3064 stream, 3016 fast_stream, 3175 short_stream)
+    TEST(Benchmark_stream_vs_fast_stream, filter_string) {
+        const size_t SIZE = static_cast<size_t>(1e7);
 
 		{
 			auto start = getCurrentTime();
@@ -239,7 +251,9 @@ namespace stream_benchmarks {
 			cout << "Time: " << diffFromNow(start) << " Short Stream" << endl;
 		}
 
-		system("pause");
+#ifdef WIN32
+        system("pause");
+#endif
 
 		ASSERT_FALSE(true);
 	}
