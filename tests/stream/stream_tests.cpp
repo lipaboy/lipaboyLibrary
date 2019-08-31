85b8e3ea356b5ab19332e74205f1acf274372a24
Artem Lipatkin <lipatkin96@gmail.com>
2019-08-31 17:30:53 +0700

    (Stream: filter) add own test file for.

--- tests/stream/stream_tests.cpp
+++ tests/stream/stream_tests.cpp
@@ -83,21 +83,6 @@ TEST_F(PrepareStreamTest, move_constructor_by_extending_the_stream) {
 //----------Other-----------//
 
 
-TEST(Stream_Filter, sample) {
-	int i = 0;
-    auto res = buildStream(1, 2, 3)
-            | filter([] (auto x) { return (x == x); })
-            | to_vector();
-
-    ASSERT_EQ(res, vector<int>({ 1, 2, 3 }));
-}
-TEST(Stream_Filter, mod3) {
-    auto res = buildStream(1, 2, 3, 4, 5, 6)
-            | filter([](int x) { return x % 3 == 0; })
-            | to_vector();
-
-    ASSERT_EQ(res, vector<int>({ 3, 6 }));
-}
 
 TEST(Stream_Get, Finite_N) {
     auto res = buildStream(1, 2, 5, 6, 1900, 234)
@@ -290,31 +275,6 @@ TEST(Stream_NTH, tempValueCopying) {
 
 	EXPECT_EQ(elem, 0);
 	ASSERT_EQ(elem2, 1);
-}
-
-int sqr (int a) { return a * a; }
-bool kekBool (int a) { return a % 2 == 0; }
-
-TEST(Stream_Filter, vector_int) {
-    vector<int> olala = { 1, 2, 3, 4, 5, 6, 7, 8 };
-    auto stream2 = buildStream(olala.begin(), olala.end());
-    auto stream4 = stream2 | filter(kekBool)
-            | map(sqr)
-            | filter([] (int a) -> bool { return a % 10 == 6; });
-    auto kek = stream4 | to_vector();
-
-    ASSERT_EQ(kek, decltype(kek)({ 16, 36 }));
-}
-
-TEST(Stream_Filter, group_by_vector) {
-    vector<int> olala = { 1, 2, 3, 4, 5, 6, 7, 8 };
-    auto stream2 = buildStream(olala.begin(), olala.end());
-    auto kek = stream2
-            | filter([] (auto a) { return a % 2 == 0; })
-            | group_by_vector(3)
-            | to_vector();
-
-    ASSERT_EQ(kek, decltype(kek)({ vector<int>({ 2, 4, 6 }), vector<int>({ 8 }) }));
 }
 
 TEST(Stream_GroupByVector, filter) {
