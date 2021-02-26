9fb7e67a078a955abdc05b9d1b8869f9d7874eba
lipatkin96 <lipatkin96@gmail.com>
2021-02-26 21:45:09 +0700

    (LongInteger) start to rewrite code for LongInteger because storage-concept of number was wrong. Write new constructors, sign method and remove minus_ field.

--- src/long_numbers/long_integer.h
+++ src/long_numbers/long_integer.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <cmath>    // std::abs
+#include <array>
 
 // building number from string
 #include <string>
@@ -14,6 +15,9 @@ namespace lipaboy_lib::long_numbers_space {
 
 	// TODO: create LongNumberBase for common methods: integralModulus, Degree, IntegralTypes, zeroIntegral
 	//			
+
+    // LongInteger
+    // Concept: use same logic as build into Integral types (signed int). Two's complement signed number representation
 
 	namespace extra {
 
@@ -42,6 +46,12 @@ namespace lipaboy_lib::long_numbers_space {
 		using IntegralType = typename TUnsignedPart::IntegralType;
 		using ResultIntegralType = typename TUnsignedPart::ResultIntegralType;
 
+        using ContainerType = std::array<IntegralType, countOfIntegrals>;
+        using iterator = typename ContainerType::iterator;
+        using reverse_iterator = std::reverse_iterator<iterator>;
+        using const_iterator = typename ContainerType::const_iterator;
+        using const_reverse_iterator = std::reverse_iterator<const_iterator>;
+
         using Sub::length;
         using Sub::zeroIntegral;
         using Sub::integralModulus;
@@ -52,27 +62,33 @@ namespace lipaboy_lib::long_numbers_space {
 		friend class LongInteger;
 
 	private:
-		LongInteger(TUnsignedPart unsignedPart, MinusType minus)
-			: unsignedPart_(unsignedPart), minus_(minus) {}
+        LongInteger(ContainerType magnitude_)
+            : magnitude(magnitude_) {}
 
 	public:
         
-        LongInteger() : unsignedPart_(), minus_(false) {}
+        LongInteger() {}
 
-		LongInteger(TSigned small) : unsignedPart_(std::abs(small)), minus_(small < 0) {}
+        LongInteger(TSigned small) {
+            magnitude[0] = IntegralType(small);
+        }
 
-		template <LengthType otherLen>
-		explicit
-			LongInteger(LongInteger<otherLen> const& other) 
-				: unsignedPart_(other.unsignedPart_), minus_(other.minus_) {}
+        // TODO: you need to take into account that sign storage at last element of array
+//		template <LengthType otherLen>
+//		explicit
+//            LongInteger(LongInteger<otherLen> const& other) {
+//                auto minLen = (other.length() > length()) ? length() : other.length();
+//                std::copy_n(other.cbegin(), minLen, std::begin(magnitude));
+//                std::fill(std::next(begin(), minLen), end(), zeroIntegral());
+//            }
 
 		explicit
 			LongInteger(std::string_view signedNumberStr, unsigned int base = 10)
 		{
 			if (signedNumberStr.length() <= 0 || base < 2)
 				LongInteger();
-			else
-				assignStr(signedNumberStr, base);
+//			else
+//				assignStr(signedNumberStr, base);
 		}
 
 		//--------------------Arithmetic operations---------------------//
@@ -80,113 +96,131 @@ namespace lipaboy_lib::long_numbers_space {
 		template <LengthType first, LengthType second>
 		using LongIntegerResult = LongInteger< extra::Max<first, second>::value >;
 
-		template <LengthType otherLen>
-		auto operator+(LongInteger<otherLen> const& other) const
-			-> LongIntegerResult<countOfIntegrals, otherLen>
-		{
-			using ResultType = LongIntegerResult<countOfIntegrals, otherLen>;
-			return (ResultType(*this) += other);
-		}
+//		template <LengthType otherLen>
+//		auto operator+(LongInteger<otherLen> const& other) const
+//			-> LongIntegerResult<countOfIntegrals, otherLen>
+//		{
+//			using ResultType = LongIntegerResult<countOfIntegrals, otherLen>;
+//			return (ResultType(*this) += other);
+//		}
 
-		template <LengthType otherLen>
-		auto operator+=(LongInteger<otherLen> const& other)
-			-> const_reference
-		{
-			// About implicit conversation from signed to unsigned:
-			// https://stackoverflow.com/questions/50605/signed-to-unsigned-conversion-in-c-is-it-always-safe
+//		template <LengthType otherLen>
+//		auto operator+=(LongInteger<otherLen> const& other)
+//			-> const_reference
+//		{
+//			// About implicit conversation from signed to unsigned:
+//			// https://stackoverflow.com/questions/50605/signed-to-unsigned-conversion-in-c-is-it-always-safe
 
-			constexpr auto MIN_LENGTH = std::min(countOfIntegrals, otherLen);
-			IntegralType carryOver(0);
-			TSignedResult carryOverSign(1);
-			constexpr TSignedResult mask(1);
-			const TSignedResult selfSign = this->signButZero();   // O(sign()) ~ O(N)
-			const TSignedResult otherSign = other.signButZero();
-			size_t i = 0;
+//			constexpr auto MIN_LENGTH = std::min(countOfIntegrals, otherLen);
+//			IntegralType carryOver(0);
+//			TSignedResult carryOverSign(1);
+//			constexpr TSignedResult mask(1);
+//			const TSignedResult selfSign = this->signButZero();   // O(sign()) ~ O(N)
+//			const TSignedResult otherSign = other.signButZero();
+//			size_t i = 0;
 
-			// TODO: hide the type-conversation into something
-			for (; i < MIN_LENGTH; i++) {
-				const TSignedResult dualRes =
-					selfSign * TSignedResult(this->unsignedPart_[i])
-					+ otherSign * TSignedResult(other.unsignedPart_[i])
-					+ carryOverSign * TSignedResult(carryOver);
+//			// TODO: hide the type-conversation into something
+//			for (; i < MIN_LENGTH; i++) {
+//				const TSignedResult dualRes =
+//					selfSign * TSignedResult(this->unsignedPart_[i])
+//					+ otherSign * TSignedResult(other.unsignedPart_[i])
+//					+ carryOverSign * TSignedResult(carryOver);
 
-				// std::abs - bad choice for extracting the result from dualRes (or not?)
-				carryOverSign =	
-					extra::sign<TSignedResult, TSignedResult>(dualRes < 0, dualRes);
+//				// std::abs - bad choice for extracting the result from dualRes (or not?)
+//				carryOverSign =
+//					extra::sign<TSignedResult, TSignedResult>(dualRes < 0, dualRes);
 				
-				this->unsignedPart_[i] = 
-					IntegralType(dualRes & TSignedResult(integralModulus()));
-				carryOver =	
-					IntegralType(mask & (dualRes >> integralModulusDegree()));
-			}
-			if constexpr (length() > MIN_LENGTH) {
-				for (; i < length(); i++) {
-					const TSignedResult dualRes =
-						selfSign * TSignedResult(this->unsignedPart_[i])
-						+ carryOverSign * TSignedResult(carryOver);
+//				this->unsignedPart_[i] =
+//					IntegralType(dualRes & TSignedResult(integralModulus()));
+//				carryOver =
+//					IntegralType(mask & (dualRes >> integralModulusDegree()));
+//			}
+//			if constexpr (length() > MIN_LENGTH) {
+//				for (; i < length(); i++) {
+//					const TSignedResult dualRes =
+//						selfSign * TSignedResult(this->unsignedPart_[i])
+//						+ carryOverSign * TSignedResult(carryOver);
 
-					this->unsignedPart_[i] =
-						IntegralType(dualRes & TSignedResult(integralModulus()));
-					carryOver = 
-						IntegralType(mask & (dualRes >> integralModulusDegree()));
-					carryOverSign =	
-						extra::sign<TSignedResult, TSignedResult>(dualRes < 0, dualRes);
-				}
-			}
-			this->setSign(TSigned(carryOverSign));
+//					this->unsignedPart_[i] =
+//						IntegralType(dualRes & TSignedResult(integralModulus()));
+//					carryOver =
+//						IntegralType(mask & (dualRes >> integralModulusDegree()));
+//					carryOverSign =
+//						extra::sign<TSignedResult, TSignedResult>(dualRes < 0, dualRes);
+//				}
+//			}
+//			this->setSign(TSigned(carryOverSign));
 
-			return *this;
-		}
+//			return *this;
+//		}
 
 	public:
-		void setSign(TSigned newSign) { minus_ = newSign < 0; }
+//		void setSign(TSigned newSign) { minus_ = newSign < 0; }
 
-		// Complexity: O(N) - because isZero() method
+        // Complexity: O(N) - because isZero() method
         TSigned sign() const {
-            return (minus_ * TSigned(-1) + !minus_ * TSigned(1)) 
-				* (!unsignedPart_.isZero());	// ~ O(N)
+            return signButZero() * (!isZero());      // ~ O(N)
         }
+
+        bool isZero() const {
+            for (auto iter = cbegin(); iter != cend(); iter++)
+                if (*iter != zeroIntegral())
+                    return false;
+            return true;
+        }
+
 	private:
 		// Complexity: O(1) - because without checking on zero value
-		TSigned signButZero() const {
-			return (minus_ * TSigned(-1) + !minus_ * TSigned(1));
-		}
+        TSigned signButZero() const {
+            const bool minus = (magnitude.back() & (1u << (integralModulusDegree() - 1)))
+                    >> (integralModulusDegree() - 1);
+            return (minus * TSigned(-1) + !minus * TSigned(1));
+        }
 
 	public:
-		LongInteger operator-() const {
-			return LongInteger(unsignedPart_, !minus_);
-		}
+//		LongInteger operator-() const {
+//			return LongInteger(unsignedPart_, !minus_);
+//		}
 
 		//----------------------------Utils------------------------------//
 
-		void assignStr(std::string_view signedNumberStr, unsigned int base = 10);
+//		void assignStr(std::string_view signedNumberStr, unsigned int base = 10);
 
-		std::string to_string(unsigned int base = 10) const {
-			return ((sign() < 0) ? "-" : "") + unsignedPart_.to_string();
-		}
+//		std::string to_string(unsigned int base = 10) const {
+//            return ((sign() < 0) ? "-" : "") + unsignedPart_.to_string(base);
+//		}
+
+    protected:
+        iterator begin() { return magnitude.begin(); }
+        iterator end() { return magnitude.end(); }
+        reverse_iterator rbegin() { return magnitude.begin(); }
+        reverse_iterator rend() { return magnitude.end(); }
+        const_iterator cbegin() const { return magnitude.cbegin(); }
+        const_iterator cend() const { return magnitude.cend(); }
+        const_reverse_iterator crbegin() const { return magnitude.crbegin(); }
+        const_reverse_iterator crend() const { return magnitude.crend(); }
 
 	private:
-		TUnsignedPart unsignedPart_;
-		MinusType minus_;
+        ContainerType magnitude;
 	};
 
-	template <LengthType length>
-	void LongInteger<length>::assignStr(std::string_view signedNumberStr, unsigned int base) {
-		if (base > 1) {
-			std::string_view signedNumberStrView = signedNumberStr;
-			signedNumberStrView.remove_prefix(
-				cutOffLeftBorder<int>(0, int(signedNumberStrView.find_first_not_of(" ")))
-			);
+//	template <LengthType length>
+//	void LongInteger<length>::assignStr(std::string_view signedNumberStr, unsigned int base) {
+//		if (base > 1) {
+//			std::string_view signedNumberStrView = signedNumberStr;
+//			signedNumberStrView.remove_prefix(
+//				cutOffLeftBorder<int>(0, int(signedNumberStrView.find_first_not_of(" ")))
+//			);
 
-			// TODO: add exception for zero length
-			if (signedNumberStrView.length() > 0) {
-				minus_ = (signedNumberStrView.front() == '-') ? true : false;
-				if (minus_)
-					signedNumberStrView.remove_prefix(1);
-				unsignedPart_.assignStr(signedNumberStrView, base);
-			}
-		}
-	}
+//			// TODO: add exception for zero length
+//			if (signedNumberStrView.length() > 0) {
+//				minus_ = (signedNumberStrView.front() == '-') ? true : false;
+//				if (minus_)
+//					signedNumberStrView.remove_prefix(1);
+//				unsignedPart_.assignStr(signedNumberStrView, base);
+//			}
+//		}
+//	}
 
 }
 
--- tests/long_numbers/long_integer_tests.cpp
+++ tests/long_numbers/long_integer_tests.cpp
@@ -19,75 +19,90 @@ namespace long_numbers_tests {
 	//----------------------------to_string()----------------------------//
 	//-------------------------------------------------------------------//
 
-	TEST(LongInteger, sign) {
-		LongInteger<1> first(-5);
-		EXPECT_TRUE(first.sign() < 0);
+    TEST(LongInteger, check) {
+
+//        LongInteger<1> first(-2);
+//        EXPECT_TRUE(first.sign() < 0);
+
+    }
+
+    TEST(LongInteger, sign) {
+        LongInteger<1> first(-5);
+        EXPECT_TRUE(first.sign() < 0);
 		
-		LongInteger<1> second(0);
-		EXPECT_TRUE(second.sign() == 0);
-	}
+        LongInteger<1> second(0);
+        EXPECT_TRUE(second.sign() == 0);
+    }
 
-	TEST(LongInteger, revert_sign) {
-		LongInteger<1> first(-5);
-		first = -first;
-		EXPECT_TRUE(first.sign() > 0);
+//	TEST(LongInteger, revert_sign) {
+//		LongInteger<1> first(-5);
+//		first = -first;
+//		EXPECT_TRUE(first.sign() > 0);
 
-		LongInteger<1> second(0);
-		second = -second;
-		EXPECT_TRUE(second.sign() == 0);
-	}
+//		LongInteger<1> second(0);
+//		second = -second;
+//		EXPECT_TRUE(second.sign() == 0);
+//	}
 
-	TEST(LongInteger, copy_constructor_different_length) {
-		LongInteger<1> first(-5);
-		LongInteger<2> second(first);
+//	TEST(LongInteger, copy_constructor_different_length) {
+//		LongInteger<1> first(-5);
+//		LongInteger<2> second(first);
 
-		EXPECT_TRUE(second.sign() < 0);
-	}
+//		EXPECT_TRUE(second.sign() < 0);
+//	}
 
-	TEST(LongInteger, construct_from_string) {
-		LongInteger<1> first("  -5");
-		EXPECT_TRUE(first.sign() < 0);
+//	TEST(LongInteger, construct_from_string) {
+//		LongInteger<1> first("  -5");
+//		EXPECT_TRUE(first.sign() < 0);
 
-		LongInteger<2> second("123456789012");
-		EXPECT_EQ(second.to_string(), "123456789012");
-		second = -second;
-		EXPECT_EQ(second.to_string(), "-123456789012");
-	}
+//		LongInteger<2> second("123456789012");
+//		EXPECT_EQ(second.to_string(), "123456789012");
+//		second = -second;
+//		EXPECT_EQ(second.to_string(), "-123456789012");
+//	}
 
-	TEST(LongInteger, sum_single_lengths) {
-		LongInteger<1> first(1);
-		LongInteger<1> second(2);
-		EXPECT_EQ((first += second).to_string(), "3");
+//	TEST(LongInteger, sum_single_lengths) {
+//		LongInteger<1> first(1);
+//		LongInteger<1> second(2);
+//		EXPECT_EQ((first += second).to_string(), "3");
 
-		first.assignStr("-2");
-		EXPECT_EQ((first + second).to_string(), "0");
-	}
+//		first.assignStr("-2");
+//		EXPECT_EQ((first + second).to_string(), "0");
+//	}
 
-	TEST(LongInteger, sum_double_lengths) {
-		LongInteger<2> first(" 3000000001");
-		LongInteger<2> second("3000000001");
-		EXPECT_EQ((first += second).to_string(), "6000000002");
+//	TEST(LongInteger, sum_double_lengths) {
+//		LongInteger<2> first(" 3000000001");
+//		LongInteger<2> second("3000000001");
+//		EXPECT_EQ((first += second).to_string(), "6000000002");
 
-		second = -second;
-		EXPECT_EQ((first += second).to_string(), "3000000001");
-		EXPECT_EQ((first + second).to_string(), "0");
+//		second = -second;
+//		EXPECT_EQ((first += second).to_string(), "3000000001");
+//		EXPECT_EQ((first + second).to_string(), "0");
 
-		// CRASH TEST
-		first = 0;
-//		EXPECT_EQ((first + second).to_string(), "-3000000001");
-	}
+//		// CRASH TEST
+//		first = 0;
+////		EXPECT_EQ((first + second).to_string(), "-3000000001");
+//	}
 
-	TEST(LongInteger, sum_diff_lengths) {
-		LongInteger<2> first(" 3000000001");
-		LongInteger<1> second(" 500000001");
-		EXPECT_EQ((first += second).to_string(), "3500000002");
+//	TEST(LongInteger, sum_diff_lengths) {
+//		LongInteger<2> first(" 3000000001");
+//		LongInteger<1> second(" 500000001");
+//		EXPECT_EQ((first += second).to_string(), "3500000002");
 
-		second = -second;
-		EXPECT_EQ((first += second).to_string(), "3000000001");
-		EXPECT_EQ((first + second).to_string(), "2500000000");
+//		second = -second;
+//		EXPECT_EQ((first += second).to_string(), "3000000001");
+//		EXPECT_EQ((first + second).to_string(), "2500000000");
 
-		//first = "                      3000000001";
-		//LongInteger<3> third("1000000003000000001")
-	}
+//		//first = "                      3000000001";
+//		//LongInteger<3> third("1000000003000000001")
+
+//        uint32_t kek = 1u;
+//        auto lol = int32_t(~kek);
+//        EXPECT_EQ(lol, -2);
+//        int64_t lol2 = int64_t(lol);
+//        EXPECT_EQ(lol2, -2);
+//        uint64_t lol3 = uint64_t(lol);
+//        EXPECT_EQ(lol3, -2);
+//	}
 
 }
